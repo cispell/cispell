dd21ddb4dbd10d7f4e221785c65e5c449b964651
khrizpv <dr_khriz@hotmail.com>
2016-02-20 20:53:09 -0500

    load env variables

--- cispell.go
+++ cispell.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"github.com/cispell/cispell/http"
+	"github.com/cispell/cispell/config"
 )
 
 func main() {
+	config.LoadEnvData()
 	http.NewServer()
 	http.InitServer()
 	http.StartServer()
